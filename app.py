0ec0de19526d02d1fb61a4f98993c2f34994476d
ian-ainebyona <ianmainebyona@gmail.com>
2026-09-06 00:56:44 +0400

    Update app.py with dynamic user inputs

--- app.py
+++ app.py
@@ -1,5 +1,23 @@
-def calculate_total(price, tax):
-    return price + (price *tax)
+def calculate_total(price: float, tax_rate: float) -> float:
+    return price + (price * tax_rate)
 
+def main():
+    print("---Sales Tax Calculator---")
+    try:
+        #capture input and convert text to decimal numbers(float)
+        
+        price = float(input("Enter item price ($)"))
+        tax_rate= float(input("Enter tax rate (e.g., 0.05 for 5%)"))
 
-print(calculate_total(100, 0.05))
+        if price <0 or tax_rate < 0:
+            print("Error: Price and tax rate must be positive numbers.")
+        else:
+            total = calculate_total(price, tax_rate)
+            #:.2f formats the number to 2 decimal places
+            print(f"Total price including tax: ${total:.2f}")
+    except ValueError:
+        print("Error: Invalid input. Please enter valid numeric values only.")
+
+if __name__ == "__main__":
+    main()
+    
